7aafacf44354d449daa8da33826874d4ba4bedba
nilesh-0001 <castreader05@gmail.com>
2025-07-18 16:14:19 +0530

    Modules and Pips

---
+++ 03_Modules and Pips/main.py
@@ -0,0 +1,13 @@
+print("Modules and Pips")
+
+# Modules and Pips 
+
+# Modules
+# There are two types of modules
+
+# External Modules - It is a Pre-written code by someone to perform any task we can just install it, But we dont know about this pre written code vey much but what it does we know very well.
+
+# Built in Modules - They are already present in the program lang , and ready to import without any install.
+
+# Pips
+# It can be used as a Pacakage manager (pip) to install .py module . It is a Package Manager.
