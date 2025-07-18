b1301b11d9364086e46fa0ddaf4dceef715ed8c5
nilesh-0001 <castreader05@gmail.com>
2025-07-18 15:20:08 +0530

    Intro of Python

---
+++ 01_Intro/main.py
@@ -0,0 +1,19 @@
+# Intro of Python 
+# Python is a High Level Programming Lang and it is also Dynamically Typed .
+# Invented by Guido Van Rossum .
+
+# Programming 
+# It is a way to tell computers to Perform any task , we use computer coz of accuraccy and speed .
+
+# Features 
+# Simple and easy to Understand .
+# It is Platform Independent and debugging was very easy , platform independent means the same code you write on linux can work on mac and Window also .
+# Good community and open source prog lang .
+# Library support is very huge .
+# It can also operate easily with other lang .
+
+# Used for 
+# Used in Data-Visulaization
+# AI and ML
+# Web-Application 
+# Handling Dataabase and etc.
