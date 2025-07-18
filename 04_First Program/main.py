e6c4f4eef67bd7cb8a34769eddc855c62afffb59
nilesh-0001 <castreader05@gmail.com>
2025-07-18 16:52:30 +0530

    First Program

---
+++ 04_First Program/main.py
@@ -0,0 +1,9 @@
+print("First Program")
+
+# Today we are gonna write our first python program by using print 
+print(10*10)
+# write a poem 
+print("Twinkle Twinkle little star")
+print("How I wonder what you are ")
+print("Up above the world so high ")
+print("Like a Diamond in the sky ")
