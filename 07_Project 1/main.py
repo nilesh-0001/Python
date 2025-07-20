d6058ab024ec2127b386560e35f1ca9587e0a5d8
nilesh-0001 <castreader05@gmail.com>
2025-07-20 21:58:12 +0530

    Operators

--- 07_Project 1/main.py
+++ 07_Project 1/main.py
@@ -1,3 +1,16 @@
 print("Project 1")
 
-# Create a clculator by using the basic the basics 
+# Create a clculator by using the basic the basics I have Learned and the calculator have to do addition , subtraction , Divison , Multiplication
+
+# But First I have to learn about the Operator things first 
+
+# Operator 
+
+# Arithmetic Operator 
+# Addition --   +
+# Subtraction --   -
+#  Exponential --  **
+# Divison --   /
+#  fllor Divison --  //
+# Modulus --   %
+
