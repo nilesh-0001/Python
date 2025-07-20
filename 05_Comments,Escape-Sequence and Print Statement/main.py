6e395e2dafd1708d18974181dadf37f4ea4bffc8
nilesh-0001 <castreader05@gmail.com>
2025-07-20 21:07:06 +0530

    Comment, Escape sequence and Print Statement

---
+++ 05_Comments,Escape-Sequence and Print Statement/main.py
@@ -0,0 +1,23 @@
+print("Comments,Escape-Sequence and Print Statement")
+
+# Comments
+# Computer is a line or code that the programmer doest want to execute, It was just for informational purpose.
+
+# ex - 
+# Info about Nilesh 
+print("hi i am a Good Boy and my name is Nilesh ")
+# for using comment use '#' and can also use ''' ''' for multi line comment 
+
+# Escape sequence 
+# It was a character or maybe a new line we dont want to add it directly so we are gonna use escape sequence 
+# for using it use \n \"
+# ex -
+print("hi I am Nilesh \n it is a Second line ")
+print("It was about \"Nilesh\" ")
+
+# Print Statement 
+# (*values: object, sep: str | None = " ", end: str | None = "\n", file: SupportsWrite[str] | None = None, flush: Literal[False] = False) -> None
+# means 
+# ex 
+print("Nilesh" , 7 , 8 , sep= " ~ ", end="last\n")
+print ("Nishu")
