6165459c2b0afeb3cc50ef8b7a5c2e0da67b604d
nilesh-0001 <castreader05@gmail.com>
2025-07-22 23:09:01 +0530

    TypeCasting

---
+++ 08_TypeCasting/main.py
@@ -0,0 +1,32 @@
+print("Typecasting")
+
+# The Conversion of one-datatype into another datatype is called Type-casting or Type-Conversion.
+
+# a = 1
+a = "1"
+# b = 2
+b = "2"
+print(a+b)
+
+# Pyhon support a wide variety of function.
+# int()
+# float()
+# str()
+# ord()
+# hex()
+# oct()
+# tuple()
+# set()
+# list()
+# dict()
+
+# There are two type of typecasting
+
+# Explict - Conversion [ Manuall Conversion ]  The Conversion of data is manually done by the user or the programmer.
+print(int(a) + int(b))  # Example of Explicit
+
+# Implict - Conversion [ Automatic Conversion ] The Conversion of data is Automatically done by the user or the programmer.
+c = 1.56
+d = 2 
+print(c+d) # Example of Implicit
+
