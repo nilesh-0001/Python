b5773f8b920959d3ca211009855e4e3d55686a34
nilesh-0001 <castreader05@gmail.com>
2025-07-22 23:31:51 +0530

    User Input

---
+++ 09_User Input/main.py
@@ -0,0 +1,15 @@
+print("User Input")
+
+# we can take the user Input just by using input() function.
+
+variable = input("Enter your name : ")
+
+age1 = input("Enter my age : ")
+age2 = input("Enter your age : ")
+
+print("My age is " + age1 + "  and age2 is " + age2 )
+
+print(age1+age2) # It is gonna show the output 30 if the age1 is 10 and age2 is 20 and it is called Concatinatation.
+
+print(int(age1)+int(age2))
+
