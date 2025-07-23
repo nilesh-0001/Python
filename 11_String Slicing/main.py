563b70b724c99fc10c1941c765f693b5ce64009d
nilesh-0001 <castreader05@gmail.com>
2025-07-23 20:02:56 +0530

    String Slicing

---
+++ 11_String Slicing/main.py
@@ -0,0 +1,18 @@
+print("String Slicing")
+
+# String slicing and Operation on Strings
+
+me = "Nishu,Nilesh"
+
+print(len(me)) # It is used to find the length of the string.
+
+print(me[0:5]) # At the fifth place the qoma in the variable me , is not gonna Print note it. 
+print(me[:]) # It is automatically gonna Print the full length of the variable me.
+print(me[:-7]) # the negative means the full lenght(in Number) of the string - (negative no. we wrote).
+print(me[-8:-2])
+
+# Quick quiz 
+name  = "Nishu"
+print(name[-4:-2])
+
+# The output is "is" [ Reason : coz when i write -4 it means the length of the string - the no. we wrote.]
