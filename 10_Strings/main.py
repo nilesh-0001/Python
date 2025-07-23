fc2cc6f78e97ff697fb123f34915d3869fff784f
nilesh-0001 <castreader05@gmail.com>
2025-07-23 19:39:14 +0530

    Strings

---
+++ 10_Strings/main.py
@@ -0,0 +1,32 @@
+print("Strings")
+
+# In Python anything that you can enclose btw single or double quotation is called string.
+
+# ex - 
+a = "12"
+b = "Nishu"
+me = '''
+hey my name is Nilesh
+i am a fresher in Programming 
+and my primary focus is on backend
+'''
+
+# use ''' ''' tripe quotation for multi line String.
+print(me)
+
+# Accessing of characters through a string
+name  = "Nilesh"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+# Note : Index start from 0 in Programming 
+
+# Looping through the string 
+# print all the characters just by using for loop
+for character in name :
+    print(character)
